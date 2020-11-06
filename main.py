e9601f062321c6acf34752343faf41f4d1a77eb6
Ricardo Rosa <ricardo.rosa@grupocriar.com.br>
2020-11-06 09:34:17 -0300

    Proximos Jogos

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ def main():
     print("****** JOGO EM PYTHON ******")
     print("****************************")
 
-    jogo = int(input("Jogos:\n1 adivinhação\n"
-                      "2 forca\n"
+    jogo = int(input("Jogos:\n1 Adivinhação\n"
+                      "2 Forca\n"
+                     "3 Pedra, Papel e Tesoura\n"
+                     "4 Maga Sena\n"
                       "\n"
                       "Escolha o jogo:"))
 
     if jogo == 1:
         jogos = Jogos("adivinhacao", "adivinhacao.jogo_adivinhacao", "JogoAdvinhacao")
         jogos.jogar()
-    else:
+    elif jogo == 1:
         jogos = Jogos("forca", "forca.jogo_forca", "JogoForca")
         jogos.jogar()
+    else:
+        print("Não implementado")
 
 
 if __name__ == "__main__":
